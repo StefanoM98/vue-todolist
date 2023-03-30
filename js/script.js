966bf5102d7e52eb124994037976b2dfa9c210d2
StefanoM98 <smartino030@gmail.com>
2023-03-30 14:00:30 +0200

    Add: line-through

--- js/script.js
+++ js/script.js
@@ -3,7 +3,29 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
-            
+            listToDo: [
+                {
+                    text: "Compra il latte",
+                    done: false
+                },
+                {
+                    text: "Pela le patate",
+                    done: true
+                },
+                {
+                    text: "Lava la macchina",
+                    done: true
+                },
+            ],
+            newTodo: []
+        }
+    },
+    methods: {
+        aggiungiToDo (){
+            if (this.newTodo.length > 0) {
+                this.listToDo.unshift(...this.newTodo)
+                this.newTodo= []
+            }
         }
     }
 }).mount("#app")
